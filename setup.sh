1bbfa8973fb5049e647997cf4ed05fc36d0f1f4d
giahuytran4205 <tghuy23@clc.fitus.edu.vn>
2026-01-04 09:50:34 +0700

    update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-. "$HOME/.cargo/env"
 apt install build-essential -y
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+. "$HOME/.cargo/env"
 
 export RUSTFLAGS='-C target-cpu=native'
 export RUSTUP_TOOLCHAIN=nightly
